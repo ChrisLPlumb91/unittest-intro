c107f8a437d7c421e96f94f789158a3e08409d83
ChrisLPlumb91 <chrislplumb91@hotmail.com>
2023-01-04 23:42:04 +0000

    Initial Commit

---
+++ evens.py
@@ -0,0 +1,16 @@
+def even_number_of_evens(numbers):
+    if isinstance(numbers, list):
+        evens = sum([1 for n in numbers if n % 2 == 0])
+
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False
+
+        return True if evens and evens % 2 == 0 else False
+    else:
+        raise TypeError('A list was not passed into the function')
+
+
+if __name__ == '__main__':
+    print(even_number_of_evens([2, 1, 4]))
